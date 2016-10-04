8a3aabd43437eba999b53b4f96d5b1f1b52a79e6
Tim Allen <tim@noblesamurai.com>
2016-10-05 10:09:04 +1100

    More permissive re spaces.

--- test/index.js
+++ test/index.js
@@ -21,4 +21,24 @@ describe('the plugin', function() {
     console.log(inspect(tree));
     expect(tree.children.length).to.equal(3); //two paragraph nodes with a whitespace node in b/w
   });
+  it('accepts multiple spaces at the end of a sentence', function() {
+    var P = require('parse-english');
+    var p = new P;
+    var plugin = require('..');
+    var inspect = require('unist-util-inspect');
+    p.use('tokenizeRoot', plugin);
+    var tree = p.parse('Mr. Henry Brown: A hapless but friendly City of London worker.   \n\n I am testing.');
+    console.log(inspect(tree));
+    expect(tree.children.length).to.equal(3); //two paragraph nodes with a whitespace node in b/w
+  });
+  it('accepts multiple spaces at the start of a sentence', function() {
+    var P = require('parse-english');
+    var p = new P;
+    var plugin = require('..');
+    var inspect = require('unist-util-inspect');
+    p.use('tokenizeRoot', plugin);
+    var tree = p.parse('Mr. Henry Brown: A hapless but friendly City of London worker.\n\n  I am testing.');
+    console.log(inspect(tree));
+    expect(tree.children.length).to.equal(3); //two paragraph nodes with a whitespace node in b/w
+  });
 });
