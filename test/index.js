59d45d1358a49f0f1f1d5bdd71b5a727e4feb437
Tim Allen <tim@noblesamurai.com>
2016-10-05 10:10:15 +1100

    more tests

--- test/index.js
+++ test/index.js
@@ -41,4 +41,14 @@ describe('the plugin', function() {
     console.log(inspect(tree));
     expect(tree.children.length).to.equal(3); //two paragraph nodes with a whitespace node in b/w
   });
+  it('handles multiple paragraphs', function() {
+    var P = require('parse-english');
+    var p = new P;
+    var plugin = require('..');
+    var inspect = require('unist-util-inspect');
+    p.use('tokenizeRoot', plugin);
+    var tree = p.parse('Mr. Henry Brown: A hapless but friendly City of London worker.\n\n  I am testing.\n\nI am testing.');
+    console.log(inspect(tree));
+    expect(tree.children.length).to.equal(5); //three paragraph nodes with two whitespace nodes in b/w
+  });
 });
